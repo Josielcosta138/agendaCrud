43debaba37de09072bb32d3e26b2eb6c1cb5f673
Josielcosta138 <cjosiel2@gmail.com>
2022-03-27 00:19:21 -0300

    Criação do repository do git

---
+++ agenda/WebContent/scripts/validador.js
@@ -0,0 +1,21 @@
+/**
+ * Validação de formulario
+ * @author josiel Costa
+ */
+
+function validar () {
+	let nome = frmContato.nome.value
+	let fone = frmContato.fone.value
+	if(nome === "") {
+		alert('Preencha o campo nome')
+		frmContato.nome.focus()
+		return false
+	} else if (fone === "") {
+		alert('Preencha o campo Fone')
+		frmContato.nome.focus()
+		return false
+	} else {
+		document.forms["frmContato"].submit()
+	}
+		
+}
